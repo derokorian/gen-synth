7114d25246da3f34adfb0ffd68464f2fca12df47
Ryan Pallas <derokorian@gmail.com>
2016-02-17 21:20:24 -0700

    use the class....

--- src/lang/mysql.php
+++ src/lang/mysql.php
@@ -43,6 +43,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  ************************************************************************************/
+use Dero\GenSynth\GenSynth;
 
 $language_data = [
     'LANG_NAME'              => 'MySQL',
